f341df64889b4b3cad9c59f7c24f4104a41b09e3
Shrabani Mishra <shrabani082002@gmail.com>
2026-09-24 10:15:44 +0530

    Learn useReducer optimize  counter example

--- src/components/hooks/useReducer/index.jsx
+++ src/components/hooks/useReducer/index.jsx
@@ -3,23 +3,44 @@ import { useReducer } from "react"
 // useReducer is a React Hook used to manage state when state logic becomes more complex.
 
 export const ReducerComp=()=>{
+  const initialState={
+    count:0,
+  }
   const reducer=(state,action)=>{
 // console.log(state,action);
-if(action.type==="INCREMENT"){
-  return state+1;
-}
-if(action.type==="DECREMENT"){
-  return state-1;
+// if(action.type==="INCREMENT"){
+//   return state+1;
+// }
+// if(action.type==="DECREMENT"){
+//   return state-1;
+// }
+// if(action.type==="RESET"){
+//   return (state=0);
+// }
+
+switch(action.type){
+  case "INCREMENT":
+    return{count:state.count+1};
+
+     case "DECREMENT":
+    return{count:state.count-1};
+
+      case "RESET":
+    return  ({count:0});
+
+  default: 
+  return state;
 }
   }
   // const[count,setCount]=useState(0);
-  const[count,dispatch]=useReducer(reducer,0);
+  const[state,dispatch]=useReducer(reducer,initialState);
   // console.log(useReducer(reducer,0));
   return(
     <div className="p-4 h-lvh flex flex-col justify-center items-center">
-    <h1>{count}</h1>
+    <h1>{state.count}</h1>
     <button onClick={()=>dispatch({type:"INCREMENT"})} className="bg-blue-500 text-white px-4 py-2 rounded m-2">Increment</button>
      <button onClick={()=>dispatch({type:"DECREMENT" })} className="bg-red-500 text-white px-4 py-2 rounded m-2">Decrement</button>
+      <button onClick={()=>dispatch({type:"RESET" })} className="bg-red-500 text-white px-4 py-2 rounded m-2">Reset</button>
     </div>
   )
 }
